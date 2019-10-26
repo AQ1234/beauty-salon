5d2e8a982932c9f1fc696c8c215c11f9c3d67f2f
AQ <pawelukasiewicz2013@gmail.com>
2019-10-26 21:49:56 +0200

    Changed hamburgermenu in phonees devices

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,23 +1,23 @@
 //Slide
-var slideIndex = 0;
-showSlides(slideIndex);
+// var slideIndex = 0;
+// showSlides(slideIndex);
 
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("main-slideshow__slide");
+// function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("main-slideshow__slide");
 
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
 
-    if (slideIndex > slides.length) {slideIndex = 1}
+//     if (slideIndex > slides.length) {slideIndex = 1}
 
    
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 20000); // Change image every 2 seconds
-}
+//     slides[slideIndex-1].style.display = "block";
+//     setTimeout(showSlides, 20000); // Change image every 2 seconds
+// }
 
 
 
