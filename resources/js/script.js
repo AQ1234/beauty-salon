17d5e7049157aefa80c475aa13f9e76f167fc433
AQ <pawelukasiewicz2013@gmail.com>
2019-11-09 19:17:51 +0100

    added changes

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,23 +1,23 @@
 //Slide
-// var slideIndex = 0;
-// showSlides(slideIndex);
+var slideIndex = 0;
+showSlides(slideIndex);
 
 
-// function showSlides(n) {
-//     var i;
-//     var slides = document.getElementsByClassName("main-slideshow__slide");
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("main-slideshow__slide");
 
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     slideIndex++;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
 
-//     if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex > slides.length) {slideIndex = 1}
 
    
-//     slides[slideIndex-1].style.display = "block";
-//     setTimeout(showSlides, 20000); // Change image every 2 seconds
-// }
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 20000); // Change image every 2 seconds
+}
 
 
 
@@ -36,8 +36,45 @@ const handleClick = () => {
 }
 
 
-hamburger.addEventListener('click', handleClick);
+ hamburger.addEventListener('click', handleClick);
 
 
 
 
+// const sign = document.querySelector('.sign');
+// const list = document.querySelector('.head__nav--list');
+
+function signClick() {
+
+  document.toggle('.head__nav--list').style.display = "block";
+}
+
+function myFun() {
+
+ var head = document.getElementById("headList");
+  
+
+
+
+ if (head.style.opacity === "0") {
+
+
+setTimeout(() => {
+  head.style.opacity = "1";
+},200);
+
+
+ } else {
+   
+  setTimeout(() => {
+  head.style.opacity = "0";
+},100);
+
+if(window.innerWidth <= 600) {
+  head.style.opacity === "1";
+}
+ }
+
+
+
+}
