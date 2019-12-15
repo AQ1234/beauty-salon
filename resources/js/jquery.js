19c87939ea8173ce40a238a74da8202c40bdf32c
Michal S <m.siemienowicz.m@gmail.com>
2019-12-15 01:47:19 +0100

    dodałem notatki

---
+++ resources/js/jquery.js
@@ -0,0 +1,4 @@
+/**
+ * Wywal ten plik Paweł jeśli wogle tego nie używasz,
+ * i jeżeli nie używasz jq to też wywal bo to gówno :)
+ */
--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,9 @@
 //Slide
 
+/**
+ * TEN PLIK JEST BARDZO KRÓTKI A MOZE BYC DOBRYM ĆWICZENIEM
+ * NAPISZ TO TAK ŻEBY BYŁO CZYSTO i ŻEBY DAŁO SIĘ TO CZYTAĆ
+ */
 showSlides = () => {
    
   let slides = document.getElementsByClassName("main-slideshow__slide");
@@ -27,16 +31,16 @@ const nav = document.querySelector('#slidebar');
 const nav2 = document.querySelector('#slidebar');
 
 const handleClick = () => {
-hamburger.classList.toggle('hamburger--active');
-nav.classList.toggle('treatments__navigation--active');
-nav.classList.toggle('pricelist__navigation--active');
+    hamburger.classList.toggle('hamburger--active');
+    nav.classList.toggle('treatments__navigation--active');
+    nav.classList.toggle('pricelist__navigation--active');
 }
 
 hamburger.addEventListener('click', handleClick);
 
 
-//Apear and Disapear menu in mobile
-
+//Apear and Disapear menu in mobile DRY!!!!!!!
+// TO RÓWNIE DOBRZE MOŻNA ZROBIĆ POPRZEZ ANIMATION w CSS
 function headNavAppear() {
 
 var head = document.getElementById("headList");
@@ -63,20 +67,20 @@ head.style.opacity = "0";
 
 
 
-/*Dropdownlist appear*/ 
-
+/*Dropdownlist appear DRY!! */ 
+// TO RÓWNIE DOBRZE MOŻNA ZROBIĆ POPRZEZ ANIMATION w CSS
 //Menu treatments
 function treatmentsApper() {
   var listTreat = document.getElementById('listTreatments');
-
+    var timeOuts = {};
   if (listTreat.style.opacity === "0") {
 
-    setTimeout(() => {
+    timeOuts.time = setTimeout(() => {
       listTreat.style.opacity = "1";
     },200);
     
      } else {
-      setTimeout(() => {
+    timeOuts.time = setTimeout(() => {
         listTreat.style.opacity = "0";
     },100);
     
@@ -84,27 +88,69 @@ function treatmentsApper() {
       listTreat.style.opacity === "1";
         }
      }
+
+     //obserwuj
+    console.log(timeOuts);
 }
 
+//DRY!! 
+// I możesz używać do woli w htmlu czy nawet tu poprzez addeventlistner
+// przekazujesz string-a
+// to i tak kupa 
+function changeOpacity(elemntID = '', time = 200) {
+    var element = document.getElementById(elemntID);
+    if(window.innerWidth <= 600) {
+        element.style.opacity === "1";
+    } else {
+        if (element.style.opacity === "0") {
+            setTimeout(() => {
+                element.style.opacity = "1";
+            },time);
 
-//Menu packets 
+        } else {
+            setTimeout(() => {
+                element.style.opacity = "0";
+            },time);
+        }
+    }
+};
+
+//Menu packets DRY !!!
+// PRZEMYŚL TO
 function packetsApper() {
-var listPack = document.getElementById('listPackets');
+    var listPack = document.getElementById('listPackets');
 
-if (listPack.style.opacity === "0") {
+    if(window.innerWidth <= 600) {
+        listPack.style.opacity === "1";
+    } else {
+        if (listPack.style.opacity === "0") {
+            setTimeout(() => {
+                listPack.style.opacity = "1";
+            },200);
 
-  setTimeout(() => {
-    listPack.style.opacity = "1";
-  },200);
-  
-   } else {
-    setTimeout(() => {
-      listPack.style.opacity = "0";
-  },100);
-  
-  if(window.innerWidth <= 600) {
-    listPack.style.opacity === "1";
-      }
-   }
+        } else {
+            setTimeout(() => {
+                listPack.style.opacity = "0";
+            },100);
+        }
+    }
+
+
+        // if (listPack.style.opacity === "0") {
+
+        //     setTimeout(() => {
+        //         listPack.style.opacity = "1";
+        //     },200);
+
+        //  } else {
+        //     setTimeout(() => {
+        //         listPack.style.opacity = "0";
+        //     },100);
+
+        //     if(window.innerWidth <= 600) {
+        //         listPack.style.opacity === "1";
+        //     }
+        // }
 }
+
 
